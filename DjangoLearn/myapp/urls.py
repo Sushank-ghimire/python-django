52445d3cab237a985b3c5057213a292eeaa1a3da
ghimiresk <ghimiresushank08@gmail.com>
2025-03-22 20:17:07 +0545

    Add app_store view and form for app variety selection; create app_store template

--- DjangoLearn/myapp/urls.py
+++ DjangoLearn/myapp/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.app, name="app"),
     path('data/', views.myapp_data, name='app_data'),
-    path('data/<int:course_id>/', views.details_page, name="course_detail")
+    path('data/<int:course_id>/', views.details_page, name="course_detail"),
+    path('app_store/', views.app_store, name="app_store")
 ]
--- DjangoLearn/myapp/views.py
+++ DjangoLearn/myapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import AppVariety
 from django.shortcuts import get_object_or_404
+from .forms import AppVarietyForm
 
 # Create your views here.
 def app(request):
@@ -13,3 +14,17 @@ def myapp_data(request):
 def details_page(request, course_id):
     app = get_object_or_404(AppVariety, pk=course_id)
     return render(request, "details.html", { 'data' : app })
+
+def app_store(request):
+    stores = None
+    if request.method == 'POST':
+        form = AppVarietyForm(request.POST)
+        if form.is_valid():
+            form.cleaned_data['app_variety']
+            app_variety = AppVariety.objects.all()
+            stores = app_variety
+    else:
+        form = AppVarietyForm()
+    if request.method == "GET":
+        return render(request, 'app_store.html', { 'stores': stores, 'form': form })
+    return render(request, 'app_store.html', { 'stores': stores, 'form': form })
